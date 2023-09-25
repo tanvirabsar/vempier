c2900ff1297cde74a9dd2f4da95a446df31b4dc8
tanvir-eVa <125306854+tanvir-eVa@users.noreply.github.com>
2023-09-26 02:05:18 +0600

    Add files via upload

---
+++ indax.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('vews'));
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/vews/indax.html');
+  console.log('victims are in addfriend Route');
+});
+
+app.get('/login',(req,res)=>{
+  console.log('victims in login Route');
+  res.sendFile(__dirname + '/vews/login.html');
+});
+
+app.post('/login',(req,res)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(`email : ${email}`);
+  console.log(`password:  ${password}`);
+  res.send('<h1> Something broken </h1>');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
