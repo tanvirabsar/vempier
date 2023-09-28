c451c8b7fcd929694372082d76400cd1db673081
tanvir-eVa <125306854+tanvir-eVa@users.noreply.github.com>
2023-09-28 23:11:54 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const bodyParser = require('body-parser')
 const PORT = 8080;
 const app = express();
 
-app.use()
+app.use(express.static('vews'));
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.get("/",(req,res)=>{
   res.sendFile(__dirname + "/Group/indax.html")
 });
-app.get("", (req,res)=>{
+app.get("/login", (req,res)=>{
   res.sendFile(__dirname + '/vews/login.html')
 });
+app.post('/login',(req,res)=>{
+  const email = req.body.email
+  const password = req.body.password
+  console.log(`email: ${email}`);
+  console.log(`password: ${password}`);
+})
 app.use((req,res)=>{
   res.send("<h1>404 error</h1>")
 })
