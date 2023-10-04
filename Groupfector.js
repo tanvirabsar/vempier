f77534638bd4029e2dbb41fa0889806f1cfdcad8
tanvir-eVa <125306854+tanvir-eVa@users.noreply.github.com>
2023-10-04 12:33:55 +0600

    Update Groupfector.js

--- Groupfector.js
+++ Groupfector.js
@@ -12,7 +12,7 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/login',(req,res)=>{
-  res.sendFile(__dirname + '/ews/login.html');
+  res.sendFile(__dirname + '/vews/code.html');
   
 });
 app.get('/code',(req,res)=>{
